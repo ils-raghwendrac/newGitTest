79e76a9992ae1036570a52522300478a62841ff2
HARISH-PC\Shree Ram <r.chouhan@thesynapses.com>
2021-04-15 17:40:21 +0530

    COMMITED SUCCESFULLY

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Sum
+{
+public static void main(String[]args)
+{
+int a,b,c;
+Scanner s = new Scanner(System.in);
+System.out.println("enter any two number");
+a=s.nextInt();
+b=s.nextInt();
+c=a+b;
+System.out.println("sum of two numbers="+c);
+}
+}
